54da14ff9be0558fc8fe0282e75d13728244ba09
adroullier <arndt@nive.co>
2013-10-13 14:49:37 +0200

    added i18n

---
+++ nive_userdb/i18n.py
@@ -0,0 +1,9 @@
+"""
+use as import:
+from nive_userdb.i18n import _
+"""
+from translationstring import TranslationStringFactory
+_ = TranslationStringFactory('nive_userdb')
+
+from nive.i18n import translator
+from nive.i18n import translate
